11f0e2a0ec2656ff33b2a3f3527ac9fddb937260
matheus.teixeira7 <matheus.teixeira7@gmail.com>
2021-09-04 14:48:42 -0300

    Alterações na página Booster

--- src/components/Pages/Booster/RightDirection/index.tsx
+++ src/components/Pages/Booster/RightDirection/index.tsx
@@ -5,6 +5,8 @@ import {
   ButtonPrimary,
   ButtonSecondary,
   Container,
+  Content,
+  ImageContainer,
   Section,
 } from './styles';
 
@@ -12,34 +14,24 @@ export const RightDirection = () => {
   return (
     <Section>
       <Container>
-        <h3>
-          <span>
-            Acelere na direção certa,
-            <br />
-          </span>
-          com foco no que importa para você.
-        </h3>
-        <p>
-          O Booster é um programa para quem já domina os fundamentos da
-          programação.
-          <br />
-          <br />
-          Nós vamos a fundo em ReactJS, através de um programa desenhado para
-          ser muito prático, aplicando uma metodologia que vai te preparar para
-          os desafios do mundo real.
-          <br />
-          <br />
-          Além disso, o programa também conta com uma trilha para você
-          desenvolver habilidades comportamentais, para te ajudar a se destacar
-          no mercado.
-          <br />
-          <br />
-        </p>
+        <Content>
+          <h3>
+            <span>Acelere na direção certa, </span>
+            com foco no que importa para você.
+          </h3>
+          <p>
+            O Booster é um programa para quem já domina os fundamentos da
+            programação.
+          </p>
+          <BoosterHeaderButtons>
+            <ButtonPrimary type="button">Inscreva-se</ButtonPrimary>
+            <ButtonSecondary type="button">Saiba mais</ButtonSecondary>
+          </BoosterHeaderButtons>
+        </Content>
 
-        <BoosterHeaderButtons>
-          <ButtonPrimary type="button">Inscreva-se</ButtonPrimary>
-          <ButtonSecondary type="button">Saiba mais</ButtonSecondary>
-        </BoosterHeaderButtons>
+        <ImageContainer>
+          <div />
+        </ImageContainer>
       </Container>
     </Section>
   );
--- src/components/Pages/Booster/RightDirection/styles.ts
+++ src/components/Pages/Booster/RightDirection/styles.ts
@@ -1,19 +1,30 @@
 import styled from 'styled-components';
 
 export const Section = styled.section`
-  border-top: 1px solid #29292e;
-  border-bottom: 1px solid #29292e;
+  border: 1px solid #29292e;
+  padding: 3.5rem 0;
   background: #1a1822;
 `;
 
 export const Container = styled.div`
-  max-width: 1120px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
   margin: 0 auto;
-  padding: 6.8rem 2rem;
+  max-width: 1120px;
+`;
+
+export const Content = styled.div`
+  padding-left: 2rem;
+  max-width: 530px;
 
   h3 {
+    font-size: 3rem;
+    line-height: 4rem;
     span {
       background: linear-gradient(to right, #61dcfb, #619ffb);
+      background-clip: text;
       -webkit-text-fill-color: transparent;
       -webkit-background-clip: text;
     }
@@ -28,7 +39,11 @@ export const Container = styled.div`
 `;
 
 export const BoosterHeaderButtons = styled.div`
-  margin: 2rem 0 4.5rem;
+  align-items: center;
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 2rem;
+  width: 100%;
 `;
 
 export const ButtonPrimary = styled.button`
@@ -38,8 +53,9 @@ export const ButtonPrimary = styled.button`
   font-size: 1.25rem;
   font-weight: 700;
   height: 4rem;
-  padding: 0 4rem;
+  padding: 0 3.5rem;
   transition: all 200ms ease;
+  white-space: nowrap;
 
   &:hover {
     filter: brightness(0.8);
@@ -54,20 +70,37 @@ export const ButtonSecondary = styled.button`
   font-size: 1.25rem;
   font-weight: 700;
   height: 4rem;
-  padding: 0 4rem;
   margin-left: 1rem;
+  padding: 0 3.5rem;
+  transition: all 200ms ease;
+  white-space: nowrap;
 
   &:hover {
     background: var(--blue-500);
-    border-radius: 0.25rem;
     border: 1px solid transparent;
     color: var(--background);
-    font-size: 1.25rem;
-    font-weight: 700;
-    height: 4rem;
-    padding: 0 4rem;
-    transition: all 200ms ease;
-
     filter: brightness(0.8);
+  }
+`;
+
+export const ImageContainer = styled.div`
+  background: #29292e;
+  height: 30rem;
+  width: 32rem;
+  margin-bottom: 3.5rem;
+
+  position: relative;
+
+  div {
+    height: 32rem;
+    width: 32rem;
+
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+
+    background-image: url('/images/rightDirection.jpg');
+    background-repeat: no-repeat;
+    background-position: center top;
   }
 `;
